af0f674ef7e4ef1dead12932547485085bd40ff8
Gael RAUX <garaux@etud.rcisima.isima.fr>
2015-11-19 15:28:35 +0100

    Redirection

---
+++ crc/cible.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html >
+	<head>
+		<meta charset="UTF-8">
+		<title>Login</title>
+		<link rel="stylesheet" href="../css/style.css">
+	</head>
+	<p style="color: #FFF; font-size: 50px;">YOUUUPI !!!</p>
+</html>
