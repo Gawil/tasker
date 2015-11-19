8870300a7f4a8fe7bc4104141c7cb341cecd123e
Gael RAUX <garaux@etud.rcisima.isima.fr>
2015-11-19 15:58:56 +0100

    Redirection OK

--- crc/cible.php
+++ crc/cible.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html >
-<?php
-	$userName = $_POST['userName'];
-?>
+
 	<head>
 		<meta charset="UTF-8">
 		<title>Login</title>
 		<link rel="stylesheet" href="../css/style.css">
 	</head>
-	<p style="color: #FFF; font-size: 50px;">Bienvenue <?php echo $userName; ?></p>
+	<p style="color: #FFF; font-size: 50px;">Bienvenue <?php echo $_POST['userName']; ?></p>
 </html>
